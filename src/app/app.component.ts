59fc3b4acbc06d8d8ce453899e06be11f1fa6c99
davidolaoluwa360 <d.atebisun@alustudent.com>
2021-05-09 15:16:23 +0200

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,64 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw-generator';
+  title: string = 'Password Generator';
+  password: string = '';
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  length: number = 0;
+
+  onChangeLength(value: any) {
+    let parseValue = value.target.value;
+    parseValue = Number.parseInt(parseValue);
+    if (typeof parseValue !== 'string') {
+      this.length = parseValue;
+      console.log(this.length);
+    } else {
+      alert('Please enter a number??');
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
